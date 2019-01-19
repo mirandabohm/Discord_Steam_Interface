ca5faf828c1601d43d8c2fb0ddc86332c1ff2b23
Upquark00 <miranda.bohm@yahoo.com>
2019-01-19 01:11:34 -0800

    Added functionality including interface between asynchronous check of Steam API and Discord status bot.

--- Discord_Bot.py
+++ Discord_Bot.py
@@ -1,13 +1,20 @@
 """
 Created on Tues Jan 1 20:14:40 2019
-Attempt at making Discord Bot for changing online status automatically. 
+Intuitive bot that checks status on multiple digital platforms. 
 @author: Upquark00
 """
 
+import time
+import win32gui as w
 import psutil as ps
+import subprocess as sp
 import discord as ds
 import asyncio
+import get_steam_info as gs
 
+BOT_TOKEN = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+STEAM_ID = 'XXXXXXXXXXXXXXX'
+API_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 
 # =============================================================================
 # PART I: Determines the nature of programs running on your machine. 
@@ -16,30 +23,24 @@ import asyncio
 PROGRAM_NAME = 'Steam'
 PROCESS = PROGRAM_NAME + '.exe'
 
-if PROCESS in (proc.name() for proc in ps.process_iter()):
-    steam_running = True
-    print(PROGRAM_NAME,"is currently running.")
-else:
-    steam_running = False
-    print(PROGRAM_NAME,"is not currently running.")
-
-bot_token = ''
+def check_steam_running():
+    if PROCESS in (proc.name() for proc in ps.process_iter()):
+        steam_running = True
+        print(PROGRAM_NAME,"is currently running.")
+    else:
+        steam_running = False
+        print(PROGRAM_NAME,"is not currently running.")
+    return steam_running
 
 # =============================================================================
 # If Steam is running, fetch data on Steam session. 
 # =============================================================================
 
-
-# =============================================================================
-# PART II: The BOT
-# =============================================================================
-
-game_name = 'GAME NAME' 
 client = ds.Client()
 
 @client.event
 async def on_ready():
-    '''Client done preparing data received from discord'''
+    '''Performed when client done preparing data received from Discord'''
     client.loop.create_task(status_task())
     print('Logged in as')
     print(client.user.name)
@@ -48,17 +49,39 @@ async def on_ready():
 
 async def status_task():
     while True:
-        await client.change_presence(status = ds.Status.offline, game = ds.Game(name = "XXXXX"), afk=False)
-        await asyncio.sleep(3)
-        await client.change_presence(status = ds.Status.idle, game = ds.Game(name = game_name), afk=False)
-        await asyncio.sleep(3)
+        if check_steam_running():
+            player_info = gs.get_player_info(API_KEY, STEAM_ID)
+            # Functionality to be added later
+            # player_status = gs.get_status(player_info)
+            ingame_name = gs.get_ingame_name(player_info)
+            
+            if ingame_name:
+                await client.change_presence(status = ds.Status.online, game = ds.Game(name = str(ingame_name)), afk=False)
+                await asyncio.sleep(3)
+            else:
+                await client.change_presence(status = ds.Status.online, game = ds.Game(name = 'nothing at the moment'), afk=False)
+                await asyncio.sleep(3)
         
-client.run(bot_token)
+client.run(BOT_TOKEN)
 
 
 
-
-
+# =============================================================================
+# For later
+# 
+# import time
+# 
+# running = []
+# 
+# i = 0
+# while i <10:
+#     prog = w.GetWindowText(w.GetForegroundWindow())
+#     print(prog, time.time())
+#     time.sleep(2)
+#     running.append(prog)    
+#     i += 1
+# 
+# =============================================================================
 
 
 
--- get_steam_info.py
+++ get_steam_info.py
@@ -1,20 +1,25 @@
 
 import requests
 
-steam_IDs = 'XXXXXXXXXXXXXXX'
-API_key = 'XXXXXXXXXXXXXXXXXXXXXXXXX'
+steam_ID = 'XXXXXXXXXXXXXXX'
+API_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+profile_url = 'https://steamcommunity.com/profiles/' + steam_ID
 
-url = 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key='+ API_key +'&steamids=' + steam_IDs
+def get_player_info(API_key, steam_ID):
+    url = 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=' \
+    + API_key +'&steamids=' + steam_ID
 
-resp = requests.get(url)
-
-if resp.status_code != 200:
-    raise Exception('Request unsuccessful; returned code ' + str(resp.status_code) + ': ' +
-                    str(requests.status_codes._codes[resp.status_code][0]))
-else:
-    print('Success!')
-    player_info = resp.json()['response']['players'][0]
+    resp = requests.get(url)
     
+    if resp.status_code != 200:
+        raise Exception('Request unsuccessful; returned code ' + str(resp.status_code) \
+        + ': ' + str(requests.status_codes._codes[resp.status_code][0]))
+    else:
+        print('Success! API responded to call.')
+        player_info = resp.json()['response']['players'][0]
+    return player_info
+
+def get_status(player_info):
     # personastate determines player's online status
     if player_info['personastate'] == 0:
         player_status = 'Offline'
@@ -30,3 +35,16 @@ else:
         player_status = 'Looking to trade'
     elif player_info['personastate'] == 6:
         player_status = 'Looking to play'
+    return player_status
+
+def get_ingame_name(player_info):
+    player_name = player_info['personaname']
+    try:
+        game = player_info['gameextrainfo']
+        return game
+    except KeyError:  
+        print('Player',player_name,'is not currently in a game.')
+        return False
+        
+player_info = get_player_info(API_key, steam_ID)
+ingame_name = get_ingame_name(player_info)
