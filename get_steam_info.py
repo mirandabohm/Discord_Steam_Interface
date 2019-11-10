6c9e27658539bed5f34ca26657d65d70faca91f3
Upquark00 <miranda.bohm@yahoo.com>
2019-11-10 14:36:22 -0800

    Rename Info_handler class to SteamUser, return False if no active Steam game, and generally improve readability

--- get_steam_info.py
+++ get_steam_info.py
@@ -1,27 +1,34 @@
+'''
+This module imports information from a Steam user's session.
+
+Annotated at 11/10/2019 @ 14:20:00 PDT 
+Author: Upquark00
+
+'''
 
 import requests
 
-class Info_handler():
-    '''Keep track of a player's information in real time.'''
-    def __init__(self, steam_ID = '', 
-                 API_key=''):
-        
-        self.__steam_ID = steam_ID
-        self.__API_key = API_key        
-        self.__API_url = 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=' \
-        + self.__API_key +'&steamids=' + self.__steam_ID
-        self.statuslist = {0: 'offline', 1: 'online', 3: 'dnd', 4: 'idle', 5: 'idle', 6: 'online', 7: 'online'}
-        # call status
+from credentials import API_key, steam_ID, BOT_TOKEN
 
+class SteamUser():
+    '''Create an object holding a player's account information and current status.'''
+    
+    def __init__(self, steam_ID, API_key):
+        self.API_url = 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=' \
+        + API_key +'&steamids=' + steam_ID
+        self.info = self.get_player_info()
+        self.active_game = self.get_active_game(self.info)       
+        self.statuslist = {0: 'offline', 1: 'online', 3: 'dnd', 4: 'idle', 5: 'idle', 6: 'online', 7: 'online'}
+        
+        # call status
         # profile_url = 'https://steamcommunity.com/profiles/' + steam_ID
-        
         self.__player_info = self.get_player_info()
-        self.__player_status = self.get_player_status(self.__player_info)
-        
+        self.__player_status = self.get_player_status(self.__player_info)            
+    
     def get_player_info(self):
         '''Returns a dict containing Steam information for one player using the
         Steam API. Requires the desired player's user ID and a valid API key.'''
-        self.response = requests.get(self.__API_url)
+        self.response = requests.get(self.API_url)
         
         if self.response.status_code != 200:
             raise Exception('Request unsuccessful; returned code ' + str(self.response.status_code) \
@@ -42,35 +49,43 @@ class Info_handler():
         'Snooze'            |      4      | 'Idle'
         'Looking to trade'  |      5      | 'Online'
         'Looking to play'   |      6      | 'Online'
+        
         '''
+
+        # XXX: Is 'self.__steam_state' or 'self.steam_state' preferred, considering
+        # that this variable will never called outside the function?       
         self.__steam_state = player_info_dict['personastate']
         
         self.__player_status = self.statuslist[self.__steam_state]
         return self.__player_status
 
-# Is it better to use 'self.__steam_state' since when this variable will never 
-# called outside the function? Or shall I use simply 'steam_state?'
         
-    def get_ingame_name(self, player_info_dict):
-        '''Returns the name of the Steam game player is playing. 
-        If player is logged into Steam, but not currently playing anything,
-        return False.'''
+    def get_active_game(self, player_info_dict):
+        '''Returns the name of the Steam game that the player is currently playing. 
+        If not currently playing anything, return False.'''
+        
         self.__player_name = player_info_dict['personaname']
         try:
             self.__game = player_info_dict['gameextrainfo']
             print('Player ',self.__player_name,' is playing ',self.__game,'.',sep='')
-            return self.__game
+            game_name = self.__game
         except KeyError:  
             print('Player',self.__player_name,'is not currently in a game.')
-            return False
+            game_name = False
+        return game_name 
     
     def get_player_name(self):
         return self.__player_name
 
-instance = Info_handler()
-info = instance.get_player_info()
-print(info)
-status = instance.get_player_status(info)
-print(status)
-game = instance.get_ingame_name(info)
-print(game)
+print(API_key, steam_ID, BOT_TOKEN)
+player = SteamUser(steam_ID, API_key)
+
+# =============================================================================
+# instance = SteamUser(steam_ID, API_key)
+# info = instance.get_player_info()
+# print(info)
+# status = instance.get_player_status(info)
+# print(status)
+# game = instance.get_active_game(info)
+# print(game)
+# =============================================================================
