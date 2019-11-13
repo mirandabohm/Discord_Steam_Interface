ba8c463b5e9de994f5da9225dd55a6f78a9983a7
Upquark00 <miranda.bohm@yahoo.com>
2019-11-12 16:50:56 -0800

    Add get_steam_state. Rename returned values and class attributes to improve readability.

--- get_steam_info.py
+++ get_steam_info.py
@@ -19,7 +19,8 @@ class SteamUser():
         self.info = self.get_user_info()
         self.active_game = self.get_active_game(self.info) 
         self.name = self.info['personaname']
-        self.status = self.get_player_discord_status(self.info)
+        self.steam_state = self.get_steam_state(self.info)
+        self.discord_status = self.get_player_discord_status(self.steam_state)
         self.profile_url = 'https://steamcommunity.com/profiles/' + steam_ID
         
     def get_user_info(self):
@@ -32,25 +33,24 @@ class SteamUser():
             + ': ' + str(requests.status_codes._codes[response.status_code][0]))
         else:
             print('Success! API responded to call.')
-            self.__player_info = response.json()['response']['players'][0]
-        return self.__player_info
+            player_info = response.json()['response']['players'][0]
+        return player_info
 
 # NOTE: get_player_discord_status translates the Steam status given by the Steam
-# API to a Discord status readable by the Bot. Therefore, that functionality 
-# (currently living within get_steam_info) should be moved to this module. 
+# API to a Discord status readable by the Bot. That functionality shoudl be 
+# appropriately divided such that a function in this module returns steam status, 
+# and another, separate function in Dicord_Bot.py translates this to a Discrd status. 
 
-# TODO: define a function get_steam_status which takes the player's info dict
+# TODO: define a function get_steam_state which takes the player's info dict
 # and returns the steam state for export to get_discord_status. Should this be 
 # returned as steam state or as steam status??
+    
+    def get_steam_state(self, player_info_dict):
+        '''Grabs Steam status from information provided by the Steam API. 
         
-    def get_player_discord_status(self, player_info_dict):
+        Accepts JSON information provided by the Steam API as a dictionary and 
+        returns Steam State as a number between 0 and 6:
         
-        '''Translates player's Steam status to its corresponding Discord status.
-        
-        Takes Steam user's information as a dictionary, extracts steam status 
-        code (as a number 0 through 6), and returns Discord status as a string.
-        
-        STEAM STATUS        | STEAM STATE | DISCORD STATUS
         'Offline'           |      0      | 'Offline'
         'Online'            |      1      | 'Online'
         'Busy'              |      2      | 'dnd'
@@ -60,10 +60,24 @@ class SteamUser():
         'Looking to play'   |      6      | 'Online'
         
         '''
-
-        status_list = {0: 'offline', 1: 'online', 3: 'dnd', 4: 'idle', 5: 'idle', 6: 'online', 7: 'online'}
         steam_state = player_info_dict['personastate']
-        return status_list[steam_state]
+        return steam_state
+    
+    def get_player_discord_status(self, steam_state):
+        
+        '''Translates player's Steam status to its corresponding Discord status.
+        
+        Takes Steam user's information as a dictionary, extracts steam status 
+        code (as a number 0 through 6), and returns Discord status as a string.
+        
+        STEAM STATUS        | STEAM STATE | DISCORD STATUS
+
+        
+        '''
+
+        possible_discord_statuses = {0: 'offline', 1: 'online', 3: 'dnd', 4: 'idle', 5: 'idle', 6: 'online', 7: 'online'}
+        discord_status = possible_discord_statuses[steam_state]
+        return discord_status
         
     def get_active_game(self, player_info_dict):
         '''Returns the name of the Steam game that the player is currently playing. 
